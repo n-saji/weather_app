72fc7ca1d877b4cbb90222bafca099e1e849f4a5
Nikhil Saji <nikhilsaji200@gmail.com>
2025-01-24 22:48:42 -0500

    made changes

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,6 +1,5 @@
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 const GEO_API_KEY = process.env.GEO_API_KEY;
-// export const GEO_API_KEY = "c6ef564e4fmsh4cab87d4be73e1fp1f5038jsn1e0a1fc7d18f";
 export const GEO_OPTIONS = {
   method: "GET",
   headers: {
