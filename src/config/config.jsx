9de7e488ed484ee63f507aa0f81fa4bd85e3e899
Nikhil Saji <nikhilsaji200@gmail.com>
2025-01-25 01:02:23 -0500

    secret keys

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,12 +1,11 @@
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-const GEO_API_KEY = process.env.GEO_API_KEY;
 export const GEO_OPTIONS = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": GEO_API_KEY,
+    "x-rapidapi-key": process.env.REACT_APP_GEO_API_KEY,
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = process.env.WEATHER_API_KEY
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
