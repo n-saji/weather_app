03eb35c39440364cf3e370d988f9ba5b157c8550
Nikhil Saji <nikhilsaji200@gmail.com>
2025-02-17 21:59:53 -0500

    api changes

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,3 +1,4 @@
-export const SERVER_API = "https://nodejs-bd2k.onrender.com/api";
+// export const SERVER_API = "https://nodejs-bd2k.onrender.com/api";  //RENDER
+export const SERVER_API = "https://ag5cfo4ifkuw2m7l45wthocbyy0gbmjv.lambda-url.us-east-2.on.aws/api"; //AWS
 export const LOCAL_SERVER_API = "http://localhost:5000/api";
 
